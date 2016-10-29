012cee528cf188dfc944c93b8cd87423526643dd
Tomas Barry <tomas.barry@hotmail.com>
2016-10-29 12:29:17 +0100

    Added documentation to client file

--- Client/client.js
+++ Client/client.js
@@ -1,9 +1,22 @@
-var s = require('net').Socket();
-s.connect(8000, '10.62.0.117');
-s.write('GET http://10.62.0.117/echo.php?message=hello HTTP/1.1\n\n');
+// Import node socket module
+var socket = require('net').Socket();
 
-s.on('data', function(d){
+// Set up socket connection variables
+var socketVariables = {
+    serverPort: 8000,
+    serverIPAddress: '10.62.0.117'
+};
+    
+// Set up socket connection to the server
+socket.connect(socketVariables.serverPort, socketVariables.serverIPAddress);
+
+// Send message to the echo server through the socket connection
+socket.write('GET http://' + socketVariables.serverIPAddress + '/echo.php?message=hello HTTP/1.1\n\n');
+
+// Create handler for the response from the server
+socket.on('data', function(d) {
     console.log(d.toString());
 });
 
-s.end();
+// Close the socket connection
+socket.end();
